ff4b39e22d194e7208d54434bc6d89357b7ccb29
Jerry <jerry.zhang@oocl.com>
2018-08-26 22:18:19 +0800

    [Leon,Jerry]optimize implement three letters of abc and remove if (size==2)

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -4,25 +4,17 @@ import java.util.List;
 
 public class Permutation {
     public List<String> call(String input) {
-        List<String> result = new ArrayList<>();
+        List<String> permutations = new ArrayList<>();
         if (input.length() == 1) {
-            result = Arrays.asList(input);
+            return Arrays.asList(input);
         }
-        if (input.length() == 2) {
-            for (int i = 0; i < input.length(); i++) {
-                result.add(input.charAt(i) + getRestString(input, i));
+        for (int i = 0; i < input.length(); i++) {
+            List<String> restPermutations = call(getRestString(input, i));
+            for (String permutation : restPermutations) {
+                permutations.add(input.charAt(i) + permutation);
             }
         }
-        if (input.length() == 3) {
-            List<String> result1 = new ArrayList<>();
-            for(int x=0;x<input.length();x++){
-                result1 = call(getRestString(input, x));
-                for (int j =0; j < result1.size(); j++) {
-                    result.add(input.charAt(x) + result1.get(j));
-                }
-            }
-        }
-        return result;
+        return permutations;
     }
 
     private String getRestString(String input, int index) {
